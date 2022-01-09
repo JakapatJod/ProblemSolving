c1ff1d173dd0574da3e59b8a7527f5517dc0d97e
JakapatJod <jakapat2332@gmail.com>
2022-01-09 09:54:54 +0700

    up

--- Homework/SString.py
+++ Homework/SString.py
@@ -6,7 +6,6 @@ for i in range(string):
     for j in range(string-1):
         if str[j] > str[j+1]:
             str = str[:j] + str[j+1] + str[j] + str[j+2:]
-            
 
-print("\t\tSorted String : ", str)
+print("\t\tSorted String : ", str.upper())
 print('')
